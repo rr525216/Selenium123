f31c90097ce47489f2381ef1ddc02bdb26ff4d84
naveenreddy9999 <naveenreddybandi123@gmail.com>
2022-11-24 12:00:26 +0530

    report 123

--- src/main/java/com/functions/factory/DriverFactory.java
+++ src/main/java/com/functions/factory/DriverFactory.java
@@ -23,19 +23,27 @@ public class DriverFactory {
 
 		if (browser.equals("chrome")) {
 
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("start-maximized"); // open Browser in maximized mode
-			options.addArguments("disable-infobars"); // disabling infobars
-			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
-			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-			options.addArguments("--no-sandbox"); // Bypass OS security model
+//			ChromeOptions options = new ChromeOptions();
+//			options.addArguments("start-maximized"); // open Browser in maximized mode
+//			options.addArguments("disable-infobars"); // disabling infobars
+//			options.addArguments("--disable-extensions"); // disabling extensions
+//			options.addArguments("--disable-gpu"); // applicable to windows os only
+//			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//			options.addArguments("--no-sandbox"); // Bypass OS security model
 			//WebDriver tlDriver = new ChromeDriver(options);
 //			WebDriverManager.chromedriver().setup();
 //			tlDriver.set(new ChromeDriver(options));
 
 		//	System.setProperty("webdriver.chrome.driver", ConfigReader.getConfigValue("chromepath"));
-			System.setProperty("webdriver.chrome.driver", ".\\src\\main\\java\\com\\functions\\factory\\chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver", ".\\src\\main\\java\\com\\functions\\factory\\chromedriver.exe");
+//			tlDriver.set(new ChromeDriver(options));
+
+
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--headless");
 			tlDriver.set(new ChromeDriver(options));
 
 		} else {
