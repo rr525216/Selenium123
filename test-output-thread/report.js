5ffe8607190e26cc84efcfb732825661616be44c
naveband <naveenreddybandi123@gmail.com>
2022-11-28 11:12:28 +0530

    naveen

---
+++ test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "92995416-1d13-4092-a1d1-14c1c6a6f3cc",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1669613311364,
+    "group": 1,
+    "content": "",
+    "tags": "@naveen123,@googlepage23,@navww,@123,@amazonproduct,@amazonproduct,",
+    "end": 1669613336193,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
