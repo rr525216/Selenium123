74255d32be55434f92de889bd13fa44c4e5084e7
naveband <naveenreddybandi123@gmail.com>
2022-11-30 14:38:15 +0530

    naveen

--- src/test/java/stepDef/utilities/screenShot.java
+++ src/test/java/stepDef/utilities/screenShot.java
@@ -9,11 +9,16 @@ import org.openqa.selenium.WebDriver;
 public class screenShot {
     public static WebDriver driver;
     public static void takescreenshot(String screenShotName) {
-        System.out.println(ConfigReader.getScenarioContext("Scenario"));
-        Scenario scenario = (Scenario) ConfigReader.getScenarioContext("Scenario");
-        driver = ConfigReader.getDriver("driver");
-        byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        scenario.attach(sourcePath, "image/png", screenShotName);
+        try {
+            System.out.println(ConfigReader.getScenarioContext("Scenario"));
+            Scenario scenario = (Scenario) ConfigReader.getScenarioContext("Scenario");
+            driver = ConfigReader.getDriver("driver");
+            byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(sourcePath, "image/png", screenShotName);
+        }
+        catch (Exception e){
+            System.out.println("screen shot not found");
+        }
     }
 
     public static void fieldname(String name){
