508725b4225414264dac6a9f5af8eb5d1fc0e62b
naveband <naveenreddybandi123@gmail.com>
2022-12-29 12:06:17 +0530

    firefoxOptions setup

--- src/main/java/com/functions/factory/DriverFactory.java
+++ src/main/java/com/functions/factory/DriverFactory.java
@@ -77,6 +77,14 @@ public class DriverFactory {
         }
         if (browser.equals("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("start-maximized"); // open Browser in maximized mode
+            options.addArguments("disable-infobars"); // disabling infobars
+            options.addArguments("--disable-extensions"); // disabling extensions
+            options.addArguments("--disable-gpu"); // applicable to windows os only
+            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.addArguments("--no-sandbox");
+
+            //  ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
             options.addArguments("--headless");
